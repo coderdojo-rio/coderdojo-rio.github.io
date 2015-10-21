fab88c6dd5e4820186a15a23330b61a940a24696
Diogo <diogo.mvieira@gmail.com>
2015-10-21 00:58:36 -0200

    Nova versao com analytics, disqus e traduções ok

--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -20,21 +20,28 @@ OUTPUT_PATH = '..'
 DISPLAY_PAGES_ON_MENU = True
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+PLUS_ONE = True
+DISQUS_SITENAME = 'coderdojorio'
+GOOGLE_ANALYTICS = 'UA-58403599-2'
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('facebook', 'https://www.facebook.com/coderdojorio'),
+          ('linkedin', 'https://www.linkedin.com/company/coderdojo-rio'),
+          ('github', 'https://github.com/coderdojo-rio'),
+          ('email', 'mailto:contato@coderdojorio.com.br'),
+          ('google', 'https://groups.google.com/forum/#!forum/coderdojo-rio'),
+          ('rss', '/feeds/all.atom.xml'))
 
 DEFAULT_PAGINATION = False
 
